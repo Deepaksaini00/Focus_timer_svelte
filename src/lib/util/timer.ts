a99f7eac7313ad7707f3c4d95ef30c3f2c2901ce
Deepak Saini <ds954642@gmail.com>
2025-05-24 18:06:43 +0530

    Progress Bar Added

--- src/lib/util/timer.ts
+++ src/lib/util/timer.ts
@@ -7,7 +7,7 @@ export function toSeconds (hrs:number , min:number , sec:number):number
 
 // Set Hrs , Mins , Secs using Totoal Seconds
 
-export function setTime (totalSec:number): {h:number , m: number , s:number} {
+export function fromTime (totalSec:number): {h:number , m: number , s:number} {
   const h = Math.floor(totalSec / 3600);
   const m = Math.floor((totalSec % 3600)/60);
   const s = Math.floor(totalSec % 60);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,51 @@
 <script lang="ts">
 	import {tick} from 'svelte';
-	import {toSeconds , setTime} from '$lib/util/timer'
+	import {toSeconds , fromTime} from '$lib/util/timer'
 	let hours:number = 0;
 	let minutes:number = 0;
 	let seconds:number = 0;
 	let intervalId :number| null = null;
 	let totalSeconds: number = 0;
+	let timerOn:boolean = false;
+	let startTime:number = 0;
+	let progress_timer:number=0;
 
-	function startTimer() {
-		if(!intervalId){
-			totalSeconds = toSeconds(hours, minutes , seconds);
-			intervalId =  setInterval(async()=> {
-				if(totalSeconds > 0){
-					totalSeconds--;
-					console.log("sec",totalSeconds);
-					const time = setTime(totalSeconds);
-					console.log("totaltime:", time);
-					hours = time.h;
-					minutes = time.m;
-					seconds = time.s;
-					console.log(seconds);
-					await tick()
-				}else{
-					stop()
-					alert('Focus Session Completed');
-				}
-			},1000)  
-		}
-	}
-
-	function stop(){
-		console.log("stoppppping......")
+	function stopTimer(){
+		timerOn = false;
 		if(intervalId !== null){
 			clearInterval(intervalId);
 			intervalId = null;
 		}
 	}
+
+	function startTimer() {
+		if(!intervalId){
+			totalSeconds = toSeconds(hours, minutes , seconds);
+			startTime = totalSeconds;
+			timerOn = true;
+			intervalId =  setInterval(async()=> {
+				if(totalSeconds > 0){
+					totalSeconds--;
+					const time = fromTime(totalSeconds);
+					hours = time.h;
+					minutes = time.m;
+					seconds = time.s;
+					await tick()
+					progress_timer = ((startTime - totalSeconds)/startTime)*100;
+
+				}else{
+					stop()
+
+				}
+			},1000)  
+		}
+	}
+
 </script>
 
-<div class="bg-orange-200">
+<div class="bg-orange-300">
 	<div class="flex item-center flex-col justify-center overflow-hidden h-10%">
-		<div class="bg-gray-50 item-center grid grid-flow-col justify-items-center">
+		<div class="border-b-4 border-grey-300 bg-orange-200 item-center grid grid-flow-col justify-items-center">
 			<h1 class="font-serif text-4xl font-bold">
 				Focus Time
 				<i class="fa-solid fa-bell"></i>
@@ -48,18 +53,20 @@
 		</div>
 		<div></div>
 		<form
-			on:submit|preventDefault={startTimer}
-			class="border-4 border-indigo-500 flex flex-col items-center justify-center min-h-screen flex-grow"
+			id="time-form" 
+			class="flex flex-col items-center justify-center min-h-screen flex-grow"
 		>
-			<p class="text-x1 font-bold bg-orange-300 p-2 rounded-xl">
-				Focus Session Completed :)
-			</p>
+			<div class="w-1/6 bg-gray-300 h-10 rounded border-black border-4 border-double">
+				<div class="bg-orange-500 h-full " style="width: {progress_timer}%; transition: width 0.3s;"></div>
+			</div>
+
 			<div class="h-20 max-w-fit flex items-center justify-center gap-2">
 				<input
 					bind:value={hours}
 					type="number"
 					min="0"
 					max="59"
+					disabled = {timerOn}
 					class="time-input remove-arrow w-20 border-2 border-gray-500 rounded px-4 py-2 mt-2 w-30"
 				/>
 				<span class="text-xl font-bold">:</span>
@@ -68,13 +75,16 @@
 					min="0"
 					max="59"
 					bind:value={minutes}
+					disabled = {timerOn}
 					class="time-input remove-arrow w-20 border-2 border-gray-500 rounded px-4 py-2 mt-2 w-30"
+
 				/>
 				<span class="text-xl font-semibold">:</span>
 				<input
 					type="number"
 					min="0"
 					max="59"
+					disabled = {timerOn}
 					bind:value={seconds}
 					class="time-input remove-arrow w-20 border-2 border-gray-500 rounded px-4 py-2 mt-2 w-30"
 				/>
@@ -87,7 +97,7 @@
 					>Start</button
 				>
 				<button
-					on:click={stop}
+					on:click={stopTimer}
 					class="bg-green-400 hover:bg-green-500 h-10 max-w-fit mt-10 font-bold p-2 border-2 border-black rounded-xl"
 					>Stop</button
 				>
